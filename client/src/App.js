ba6e78aaa5ffed7f97cedd7e9ea458dee8b74f62
samikshashahare <samikshashahare953@gmail.com>
2023-12-21 09:53:35 +0530

    created short url

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import "./App.css";
+import axios from 'axios';
+import copyicon from './copy-icon.png';
 function App() {
+  const [url, setUrl] = useState('');
+  const [slug, setSlug] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  // this function will work for generate link
+  const generateLink = async () => {
+    const response = await axios.post('/link', {
+      url,
+      
+      slug
+    })
+
+    // const shortUrl = response?.data?.data?.shortUrl
+    // setShortUrl(shortUrl)
+    // instead of above method we are using below short method to set short url
+
+    setShortUrl(response?.data?.data?.shortUrl)
+  };
+
+  const copyShortUrl = () => {
+
+    // this is javacript method use for copy 
+    navigator.clipboard.writeText(shortUrl);
+    alert('Copied to Clipboard!')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='app-title'>🔗Quick Links</h1>
+
+      <div className='app-container'>
+        <div className='link-generation-card'>
+          <h2>Link Generation</h2>
+
+          <input type='text'
+            placeholder='URL'
+            className='user-input'
+            value={url}
+            onChange={(e) => {
+              setUrl(e.target.value)
+            }} />
+
+          <input type='text'
+            placeholder='Slug (optional)'
+            className='user-input'
+            value={slug}
+            onChange={(e) => {
+              setSlug(e.target.value)
+            }} />
+
+<div className='short-url-container'>
+      <input
+        type='text'
+        placeholder='input-short-url'
+        className='user-input'
+        value={shortUrl}
+        disabled
+      />
+      <img
+        src={copyicon}
+        alt='copy-img'
+        className='copy-icon'
+        onClick={copyShortUrl}
+      />
     </div>
-  );
+
+          <button
+            type='button'
+            className='btn-generate-link'
+            onClick={generateLink}>
+            Do Magic🎆</button>
+        </div>
+
+        <div>
+          <h2>All Links</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
